3b8d7e6668a7da18546e2ab373fc20429fe1e43a
wolflint <marcinwolf363@gmail.com>
2017-03-17 22:10:09 +0000

    GRC Site
    Unit 28 - Assignment 2

---
+++ js/myscripts.js
@@ -0,0 +1,42 @@
+//CAROUSEL//
+var $item = $('.carousel .item');
+var $wHeight = $(window).height();
+$item.eq(0).addClass('active');
+$item.height($wHeight);
+$item.addClass('full-screen');
+
+$('.carousel img').each(function() {
+    var $src = $(this).attr('src');
+    var $color = $(this).attr('data-color');
+    $(this).parent().css({
+        'background-image': 'url(' + $src + ')',
+        'background-color': $color
+    });
+    $(this).remove();
+});
+
+$(window).on('resize', function() {
+    $wHeight = $(window).height();
+    $item.height($wHeight);
+});
+
+$('.carousel').carousel({
+    interval: 3000,
+    pause: "false"
+});
+
+//SMOOTH SCROLLING
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
